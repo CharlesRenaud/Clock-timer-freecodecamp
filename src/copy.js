780fa44cd7e412fb3a5fd40640a2b852e91c7e33
Charles Renaud <renaudcharlespro@gmail.com>
2021-11-16 23:02:33 +0100

    100%

--- src/copy.js
+++ src/copy.js
@@ -1,6 +1,5 @@
 import './App.scss';
 import react, {useState, useEffect} from 'react'
-import bip from './bip.mp3'
 
 const App = () => {
 
@@ -14,7 +13,6 @@ const App = () => {
   const [breakerStart, setBreakerStart] = useState(false)
   const [toogleBreaker, setToogleBreaker] = useState(false)
   const [breakerTemps, setBreakerTemps] = useState(300)
-  
   const [isBreaked, setIsBreaked] = useState(false)
 
 
@@ -49,7 +47,11 @@ const App = () => {
         <Session {...props}/>
       </div>
       <div className="container">
-        <Timer {...props} />
+        {!isBreaked 
+        ? 
+        <Timer {...props} /> 
+        : 
+        <Breaker {...props} />}
       </div>
 
       
@@ -60,18 +62,90 @@ const App = () => {
 
 export default App;
 
+const Breaker = (props) => {
+ 
+  console.log((props.breakLength))
+
+  const refresh = () => {
+    props.setBreakLength(5)
+    props.setSessionLength(25)
+    props.setTemps(1500)
+    props.setBreakerTemps(300)
+    props.setBreakerStart(false)
+    props.setToogleBreaker(false)
+    props.setTooglePlay(false)
+    props.setStart(false)
+    props.setIsBreaked(false)
+
+  }
+
+
+  const tooglings = () => {
+    if(props.toogleBreaker === true){
+      props.setToogleBreaker(false)
+      console.log("hello")
+    }else{
+      props.setToogleBreaker(true)
+      props.setBreakerStart(true)
+    }
+  }
+  
+  var a = Math.floor(props.breakerTemps/60); //minutes
+  var b = props.breakerTemps%60; //seconds
+    if(b === 0){
+      b = "00"
+    }else if(b < 10){
+      b = "0"+b
+    }
+    if(a ===0){
+      a = "00"
+    }
+    else if(a < 10){
+      a = "0"+a
+    }
+
+    useEffect(() => {
+      let interval = null;
+      if(props.toogleBreaker){
+       interval = setInterval(() => {
+         if(props.breakerTemps > 0){
+          props.setBreakerTemps(prev => prev -1)
+         }
+         else{
+           props.setToogleBreaker(false)
+           props.setIsBreaked(false)
+           props.setBreakerTemps(props.breakLength * 60)
+          }
+        }, 10);
+      }else if (!props.toogleBreaker && props.breakerTemps !== 0) {
+        clearInterval(interval);
+      }
+      return () => clearInterval(interval);
+    },[props.toogleBreaker, props.breakerTemps] );
+
+  return(
+    <div className="section timer">
+      <div className="timer-box">
+        <h1  id="timer-label">Break</h1>
+        <div id="time-left">{a}:{b}</div>
+      </div>
+      <div className="actions">
+        <div onClick={()=>tooglings()} id="start_stop">{props.toogleBreaker ? "Pause" : "Play"}</div>
+        <div onClick={()=>refresh()} id="reset">Refresh</div>
+      </div>
+    </div>
+  )
+
+}
+
+
 
 const Timer = (props) => {
-
 console.log(props.tooglePlay)
-
   useEffect(() => {
     if(!props.tooglePlay && !props.start){
     props.setTemps(props.sessionLength * 60)
     }
-    if(!props.toogleBreaker && !props.breakerStart){
-      props.setBreakerTemps(props.breakLength * 60)
-      }
   })
   
   const refresh = () => {
@@ -84,124 +158,63 @@ console.log(props.tooglePlay)
     props.setTooglePlay(false)
     props.setStart(false)
     props.setIsBreaked(false)
-    document.getElementById('beep').pause();
-    document.getElementById('beep').currentTime= 0;
-
   }
 
 
   const toogling = () => {
+    if(props.tooglePlay === true){
+      props.setTooglePlay(false)
 
-    if(!props.isBreaked){
-      console.log("hu")
-      if(props.tooglePlay === true){
-        props.setTooglePlay(false)
-      }
-      else{
-        props.setTooglePlay(true)
-        props.setStart(true)
-      }
+    }else{
+      props.setTooglePlay(true)
+      props.setStart(true)
     }
-    else{
-      console.log("ho")
-      if(props.toogleBreaker === true){
-        props.setToogleBreaker(false)
-      }
-      else{
-        props.setToogleBreaker(true)
-        props.setBreakerStart(true)
-      }
-      }
+  }
+ 
+
+  var a = Math.floor(props.temps/60); //minutes
+  var b = props.temps%60; //seconds
+    if(b === 0){
+      b = "00"
+    }else if(b < 10){
+      b = "0"+b
     }
-    let yo;
-    if(!props.isBreaked){  yo = props.temps}else{ yo = props.breakerTemps}
-
-      var a = Math.floor(yo/60); //minutes
-      var b = yo%60; //seconds
-        if(b === 0){
-          b = "00"
-        }else if(b < 10){
-          b = "0"+b
-        }
-        if(a ===0){
-          a = "00"
-        }
-        else if(a < 10){
-          a = "0"+a
-        }
-
-        const tooglingBreak = () => {
-          if(!props.isBreaked){
-            props.setTooglePlay(false)
-            props.setIsBreaked(true)
-            props.setToogleBreaker(true)
-            props.setTemps(props.sessionLength * 60)
-            props.setBreakerTemps(props.breakLength * 60)
-            props.setBreakerStart(true)
-            document.getElementById('beep').play();
-
-            
-          }
-          else{
-            props.setTooglePlay(true)
-            props.setIsBreaked(false)
-            props.setToogleBreaker(false)
-            props.setTemps(props.sessionLength * 60)
-            props.setBreakerTemps(props.breakLength * 60)
-            props.setBreakerStart(false)
-            document.getElementById('beep').play();
-
-          }
-          
-        }
-    
-  
+    if(a ===0){
+      a = "00"
+    }
+    else if(a < 10){
+      a = "0"+a
+    }
 
     useEffect(() => {
       let interval = null;
-      console.log("props TooglePlay" + " " +props.tooglePlay)
-      console.log("props ToogleBreak" + " " +props.toogleBreaker)
-
-      if(props.tooglePlay || props.toogleBreaker){
+      if(props.tooglePlay){
        interval = setInterval(() => {
-         if(!props.isBreaked){
-          if(props.temps > 0){
-            props.setTemps(prev => prev -1)
-           }
-           else{
-             tooglingBreak()
-            }
+         if(props.temps > 0){
+          props.setTemps(prev => prev -1)
          }
          else{
-          if(props.breakerTemps > 0){
-            props.setBreakerTemps(prev => prev -1)
-           }
-           else{
-             tooglingBreak()
-            }
-         }
-         
+           props.setIsBreaked(true)
+           props.setToogleBreaker(true)
+           props.setTemps(props.sessionLength * 60)
+           props.setBreakerTemps(props.breakLength * 60)
+          }
         }, 10);
       }else if (!props.tooglePlay && props.temps !== 0) {
         clearInterval(interval);
       }
-      else if (!props.toogleBreaker && props.breakerTemps !== 0) {
-        clearInterval(interval);
-      }
       return () => clearInterval(interval);
-    },[props.tooglePlay, props.temps, props.isBreaked, props.toogleBreaker, props.breakerTemps] );
-
+    },[props.tooglePlay, props.temps] );
 
   return(
     <div className="section timer">
       <div className="timer-box">
-        <h1  id="timer-label">{!props.isBreaked ? "Timer": "Breaker"}</h1>
+        <h1  id="timer-label">Timer</h1>
         <div id="time-left">{a}:{b}</div>
       </div>
       <div className="actions">
-        <div onClick={()=>toogling()} id="start_stop">{(props.tooglePlay || props.toogleBreaker ? "Pause" : "Play") }</div>
+        <div onClick={()=>toogling()} id="start_stop">{props.tooglePlay ? "Pause" : "Play"}</div>
         <div onClick={()=>refresh()} id="reset">Refresh</div>
-        <audio id="beep" src={bip} />
       </div>
     </div>
   )
@@ -213,14 +226,12 @@ const Session = (props) => {
   const sessionIncrement = () => {
     if(props.sessionLength < 60){
       props.setSessionLength(props.sessionLength +1)
-      props.setTemps(props.temps - 60)
 
     }
   }
   const sessionDecrement = () => {
     if(props.sessionLength > 1){
       props.setSessionLength(props.sessionLength -1)
-      props.setTemps(props.temps - 60)
     }
   }
 
@@ -240,15 +251,11 @@ const Break = (props) => {
   const breakIncrement = () => {
     if(props.breakLength < 60){
       props.setBreakLength(props.breakLength +1)
-      props.setBreakerTemps(props.breakerTemps + 60)
-
     }
   }
   const breakDecrement = () => {
     if(props.breakLength > 1){
       props.setBreakLength(props.breakLength -1)
-      props.setBreakerTemps(props.breakerTemps - 60)
-
     }
   }
 
